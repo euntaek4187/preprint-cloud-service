716d185fd5a24b92094d86c801e057b72343324b
euntaek <choiet4187@naver.com>
2023-11-03 23:29:31 +0900

    feature-add

--- preprint/models.py
+++ preprint/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Upload(models.Model):
+    upload_user = models.ForeignKey(verbose_name='유저', to= User, on_delete=models.CASCADE, null=True, blank=True)
+    upload_pw = models.CharField(verbose_name='비밀번호', max_length=4)
+    upload_date = models.DateTimeField(verbose_name='주문날짜', auto_now_add=True)
+
+class UploadFile(models.Model):
+    upload = models.ForeignKey(Upload, on_delete=models.CASCADE)
+    upload_file = models.FileField(upload_to='files/')
