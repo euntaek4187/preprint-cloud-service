06c871eb10f03e26aee31dbc568f3e756bb9bcc0
choieuntaek <choiet0225@gmail.com>
2025-04-12 15:04:16 +0900

    feat: update ip address

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,10 +1,9 @@
 from .base import *
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['43.201.116.139', 'preprintcloud.com', 'www.preprintcloud.com'] #도메인 혹은 ip를...
-CSRF_TRUSTED_ORIGINS = ['https://43.201.116.139', 'https://preprintcloud.com', 'https://www.preprintcloud.com']
+ALLOWED_HOSTS = ['54.180.109.153', 'preprintcloud.com', 'www.preprintcloud.com']
+CSRF_TRUSTED_ORIGINS = ['https://54.180.109.153', 'https://preprintcloud.com', 'https://www.preprintcloud.com']
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
@@ -20,84 +19,84 @@ DATABASES = {
 
 STATIC_ROOT = BASE_DIR / 'static'
 
-LOG_FILE = '/home/ubuntu/preprint-cloud-service/log/django.log'
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'standard': {
-            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
-        },
-    },
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler',
-        },
-        'logfile': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.TimedRotatingFileHandler',
-            'filename': LOG_FILE,
-            'when': "midnight",  # 매 자정마다
-            'backupCount': 31,
-            'formatter': 'standard',
-        },
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'standard'
-        },
-    },
-    # Loggers (where does the log come from)
-    'loggers': {
-        'repackager': {
-            'handlers': ['console', 'logfile'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-        'django': {
-            'handlers': ['console', 'logfile'],
-            'propagate': True,
-            'level': 'WARN',
-        },
-        'django.server': {
-            'handlers': ['console', 'logfile'],
-            'propagate': True,
-            'level': 'INFO',
-        },
-        'django.db.backends': {
-            'handlers': ['console', 'logfile'],
-            'level': 'WARN',
-            'propagate': False,
-        },
-        '': {
-            'handlers': ['console', 'logfile'],
-            'level': 'DEBUG',
-        },
-        'raven': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'sentry.errors': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'gunicorn.error': {
-            'level': 'INFO',
-            'handlers': ['logfile'],
-            'propagate': True,
-        },
-        'gunicorn.access': {
-            'level': 'INFO',
-            'handlers': ['logfile'],
-            'propagate': False,
-        },
-        'django.request': {
-            'handlers': ['logfile'],
-            'level': 'ERROR',
-            'propagate': False,
-        },
-    }
-}
+# LOG_FILE = '/home/ubuntu/preprint-cloud-service/log/django.log'
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': True,
+#     'formatters': {
+#         'standard': {
+#             'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+#         },
+#     },
+#     'handlers': {
+#         'null': {
+#             'level': 'DEBUG',
+#             'class': 'logging.NullHandler',
+#         },
+#         'logfile': {
+#             'level': 'DEBUG',
+#             'class': 'logging.handlers.TimedRotatingFileHandler',
+#             'filename': LOG_FILE,
+#             'when': "midnight",  # 매 자정마다
+#             'backupCount': 31,
+#             'formatter': 'standard',
+#         },
+#         'console': {
+#             'level': 'INFO',
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'standard'
+#         },
+#     },
+#     # Loggers (where does the log come from)
+#     'loggers': {
+#         'repackager': {
+#             'handlers': ['console', 'logfile'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#         },
+#         'django': {
+#             'handlers': ['console', 'logfile'],
+#             'propagate': True,
+#             'level': 'WARN',
+#         },
+#         'django.server': {
+#             'handlers': ['console', 'logfile'],
+#             'propagate': True,
+#             'level': 'INFO',
+#         },
+#         'django.db.backends': {
+#             'handlers': ['console', 'logfile'],
+#             'level': 'WARN',
+#             'propagate': False,
+#         },
+#         '': {
+#             'handlers': ['console', 'logfile'],
+#             'level': 'DEBUG',
+#         },
+#         'raven': {
+#             'level': 'DEBUG',
+#             'handlers': ['console'],
+#             'propagate': False,
+#         },
+#         'sentry.errors': {
+#             'level': 'DEBUG',
+#             'handlers': ['console'],
+#             'propagate': False,
+#         },
+#         'gunicorn.error': {
+#             'level': 'INFO',
+#             'handlers': ['logfile'],
+#             'propagate': True,
+#         },
+#         'gunicorn.access': {
+#             'level': 'INFO',
+#             'handlers': ['logfile'],
+#             'propagate': False,
+#         },
+#         'django.request': {
+#             'handlers': ['logfile'],
+#             'level': 'ERROR',
+#             'propagate': False,
+#         },
+#     }
+# }
